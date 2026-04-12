1bb8cb930b69ac6a1e0407b3f1e759026b56f195
dehobitto <dehobitto@dadgafk@gmail.com>
2026-04-12 17:03:45 +0300

    done.

--- utils/verifyEmail.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "regexp"
-
-var regEmail = regexp.MustCompile(`^[^/]+@[^/]+.[^/]+$`)
-
-func IsValidEmail(email string) bool {
-	return regEmail.MatchString(email)
-}
--- utils/verifyRepo.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "regexp"
-
-var regRepo = regexp.MustCompile(`^[^/]+/[^/]+$`)
-
-func IsValidRepo(repo string) bool {
-	return regRepo.MatchString(repo)
-}
